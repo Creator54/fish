62d6cddfbfc2379095ec5059dd2e4118f3abb8d9
Creator54 <hi.creator54@gmail.com>
2020-11-19 19:36:52 +0530

    add some aliases

--- config.fish
+++ config.fish
@@ -1,4 +1,12 @@
 function fish_greeting
 	clear
 	~/.config/fish/ufetch
-end 
+end
+
+alias view_pic "kitty +kitten icat" #for viewing images in kitty
+alias extract_frame "./.scripts/extract_frame"
+alias ralias "./.scripts/ralias"
+alias reduce "./.scripts/reduce"
+alias rpattern "./.scripts/rpattern"
+alias yt "./.scripts/yt"
+alias ytpart "./.scripts/ytpart"
