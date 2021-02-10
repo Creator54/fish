a5ea076783f43cf5165b1bc2fa552727bb7ae700
creator54 <hi.creator54@gmail.com>
2021-02-10 11:03:29 +0530

    update

--- config.fish
+++ config.fish
@@ -10,7 +10,7 @@ function i
   end
 end
 
-alias dd "cd ~/dev"
+alias d "cd ~/dev"
 alias v "$EDITOR"
 alias c "cd .."
 alias s "nix search"
@@ -18,13 +18,15 @@ alias r "nix-env --uninstall"
 alias q "nix-env -q"
 alias n "nvidia-offload"
 alias calc "eva"
+alias ufetch ".//.config/fish/scripts/ufetch"
 alias lectures "cd /run/mount/data/Lectures"
 alias ytdl "youtube-dl"
-alias vv "cd /etc/nixos"
+alias sys "cd /etc/nixos"
 alias view_pic "kitty +kitten icat" #for viewing images in kitty
-alias extract_frame "./.scripts/extract_frame"
-alias ralias "./scripts/ralias"
-alias reduce "./scripts/reduce"
-alias rpattern "./scripts/rpattern"
-alias yt "./scripts/yt"
-alias ytpart "./scripts/ytpart"
+
+#for stuff inside this dir
+set dir '~/.config/fish/scripts'
+
+for i in extract_frame ralias reduce rpattern yt ytpart 
+  alias $i "$dir/$i | bash"
+end
