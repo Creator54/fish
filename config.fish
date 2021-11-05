47394c515755931192f54c972fd159b9bc45438e
creator54 <hi.creator54@gmail.com>
2021-11-06 01:15:25 +0530

    improve functions ++, use links as default browser
    function v: get $argv if $argv is a link,
    function get: use wget for images

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@ set -gx TERM kitty
 set -gx TERMINAL kitty
 set -gx EDITOR vim
 set -gx VISUAL vim
-set -gx BROWSER brave
+set -gx BROWSER "links -g 'https://duckduckgo.com'"
 set -gx WALLPAPERS '/home/creator54/wallpapers'
 set -gx CPLUS_INCLUDE_PATH /nix/store/s6scq5f4vk7pmxbch63byqw0zhf988j8-libc++-11.1.0/include/c++/v1
 set -gx PAGER "bat"
@@ -59,6 +59,8 @@ end
 function v
 	if [ -z "$argv" ]
 		echo "Do pass a file to view !"
+	else if string match -qr "http|https" $argv
+		get $argv
 	else if [ -d "$argv" ]
 		echo Files Count: (count $argv/*); ls -sh $argv
 	else if string match -qr ".jpg|.png|.svg" $argv
@@ -267,9 +269,9 @@ end
 function get
 	if [ -z $argv ]
 		echo "You do need to pass a link to download !"
-	else if echo $argv | grep .github.com &> /dev/null
-		git clone $argv;
-	else if string match -qr ".jpg|.png|.svg|.mp3|.mp4|.mkv|.zip|.tar|.gz" $argv
+	else if string match -qr "github.com" $argv
+		git clone $argv && cd  (echo $argv |cut -d'/' -f5)
+	else if string match -qr ".mp3|.mp4|.mkv|.zip|.tar|.gz" $argv
 		axel -n 10 $argv #this makes 10 connections, thus speeds the download by 10x the general connection
 	else
 		wget -r –level=0 -E –ignore-length -x -k -p -erobots=off -np -N "$argv" and echo "Fetched $argv" or echo "Couldn't fetch !"
