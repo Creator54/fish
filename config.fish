d26fa96039c85fa1ffa3a96c2067135a90c44e98
creator54 <hi.creator54@gmail.com>
2021-11-11 18:26:31 +0530

    add blur function

--- config.fish
+++ config.fish
@@ -39,6 +39,15 @@ function line
 	printf "\n"
 end
 
+function blur
+	if [ -z $argv[1] ]
+		echo "Usage: blur 10 screenshot.jpg #10% blur"
+	else
+		convert -scale (math 100 - $argv[1])% -scale 1000% $argv[2] blurred-$argv[2];
+		v blurred-$argv[2];
+	end
+end
+
 function record
 	set name (echo (date '+%a-%F')-$count)
 	#	if string match -qr 'vcam' $argv
