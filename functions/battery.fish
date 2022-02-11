083fb753c65af8112e60bfdeadc64413a055075d
creator54 <hi.creator54@gmail.com>
2022-02-11 08:22:34 +0530

    update function: battery don't show when battery fully charged

--- functions/battery.fish
+++ functions/battery.fish
@@ -5,14 +5,16 @@ function battery
     acpi -i|head -n 1|cut -d' ' -f 4 | cut -d',' -f1
   else if string match -qr "time|left|time left|rem|remaining" $argv
     set rem (acpi -i|head -n 1|cut -d' ' -f5)
-    if [ (echo $rem|cut -d':' -f1) -eq 0 ]
-      set rem (acpi -i|head -n 1|cut -d' ' -f5|cut -d':' -f2,3| sed 's/:/m:/;s/$/s/')
-      #set rem ($rem|cut -d':' -f2,3| sed 's/:/M:/2;s/$/S/')
-    else
-      set rem (acpi -i|head -n 1|cut -d' ' -f5|sed 's/:/h:/;s/:/m:/2;s/$/s/')
-      #set rem ($rem|sed 's/:/H:/;s/:/M:/2;s/$/S/')
+    if not [ -z $rem ]
+      if [ (echo $rem|cut -d':' -f1) -eq 0 ]
+        set rem (acpi -i|head -n 1|cut -d' ' -f5|cut -d':' -f2,3| sed 's/:/m:/;s/$/s/')
+        #set rem ($rem|cut -d':' -f2,3| sed 's/:/M:/2;s/$/S/')
+      else
+        set rem (acpi -i|head -n 1|cut -d' ' -f5|sed 's/:/h:/;s/:/m:/2;s/$/s/')
+        #set rem ($rem|sed 's/:/H:/;s/:/M:/2;s/$/S/')
+      end
+      echo "REM: $rem"
     end
-    echo $rem
   else if string match -qr "info" $argv
   if [ (battery state) = "Discharging," ]
     printf " %s\n" (battery %)
