14808e6f74a56e6a7df6464a8c109a2d8d61c803
creator54 <hi.creator54@gmail.com>
2022-02-11 07:40:01 +0530

    update function: battery

--- functions/battery.fish
+++ functions/battery.fish
@@ -1,11 +1,18 @@
-#fetch battery left
 function battery
   if string match -qr "state" $argv
     acpi -i|head -n 1|cut -d' ' -f 3
   else if string match -qr "percentage|%|charge" $argv
     acpi -i|head -n 1|cut -d' ' -f 4 | cut -d',' -f1
-  else if string match -qr "time|left|time left" $argv
-    acpi -i|head -n 1|cut -d' ' -f 5
+  else if string match -qr "time|left|time left|rem|remaining" $argv
+    set rem (acpi -i|head -n 1|cut -d' ' -f5)
+    if [ (echo $rem|cut -d':' -f1) -eq 0 ]
+      set rem (acpi -i|head -n 1|cut -d' ' -f5|cut -d':' -f2,3| sed 's/:/m:/;s/$/s/')
+      #set rem ($rem|cut -d':' -f2,3| sed 's/:/M:/2;s/$/S/')
+    else
+      set rem (acpi -i|head -n 1|cut -d' ' -f5|sed 's/:/h:/;s/:/m:/2;s/$/s/')
+      #set rem ($rem|sed 's/:/H:/;s/:/M:/2;s/$/S/')
+    end
+    echo $rem
   else if string match -qr "info" $argv
   if [ (battery state) = "Discharging," ]
     printf " %s\n" (battery %)
