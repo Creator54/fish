28fc9c893a38d94c5a08ab3f6cb42703c2d761ec
creator54 <hi.creator54@gmail.com>
2022-02-15 17:10:34 +0530

    add/update functions : hs,error,fn

--- functions/hs.fish
+++ functions/hs.fish
@@ -3,16 +3,20 @@ function hs
     case '-h'
       echo "Usage: "
       echo "-h  : help"
+      echo "-c  : current generation"
       echo "-g  : list generations"
       echo "-r  : -r num, goes back num generations"
+    case '-c'
+      #cmd "home-manager generations | head -n 1 | tail -n 1"
+      home-manager generations | head -n 1 | tail -n 1
     case '-g'
       cmd home-manager generations | bat
     case '-r'
       switch $argv[2]
         case ''
-          set argv[2] 1
+          set argv[2] 2
       end
-      set gobackto (home-manager generations | head -n $argv[2]|tail -n 1)
+      set gobackto (home-manager generations | head -n (math $argv[2]+1) |tail -n 1)
       echo "Reverting to Generation:" (echo $gobackto|cut -d' ' -f5)
       cmd (echo $gobackto|cut -d' ' -f7)/activate
     case '*'
