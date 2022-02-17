757531ff690da79513626163193a2f7faaf75328
creator54 <hi.creator54@gmail.com>
2022-02-17 21:11:15 +0530

    update serve : add option to remove file

--- functions/serve.fish
+++ functions/serve.fish
@@ -1,7 +1,11 @@
 function serve
-  if [ -z $argv ]
+  if [ -z $argv[1] ]
     echo "Usage : "\n
     echo "serve file    :   hosts the file on sharedby.creator54.me"
+    echo "serve -d file :   removes the file hosted on sharedby.creator54.me"
+  else if [ $argv[1] = "-d" ]
+    echo "Removing $argv[2] .."
+    ssh $NIX -i ~/.ssh/webserver -t "rm -rf ~/website-stuff/sharedby/$argv[2] && sudo nixos-rebuild switch" && echo "File is removed !"
   else
     set url "http://sharedby.creator54.me/$argv"
     push $argv ~/website-stuff/sharedby
