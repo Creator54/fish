d7ba27673085502a15ce81b6d59fa608c5f27d8a
creator54 <hi.creator54@gmail.com>
2022-11-23 05:22:28 +0530

    update -> serve : Fixed push server and remote

--- functions/serve.fish
+++ functions/serve.fish
@@ -1,14 +1,14 @@
 function serve
   if [ -z $argv[1] ]
     echo "Usage : "\n
-    echo "serve file    :   hosts the file on sharedby.creator54.me"
-    echo "serve -d file :   removes the file hosted on sharedby.creator54.me"
+    echo "serve file    :   hosts the file on bin.creator54.me"
+    echo "serve -d file :   removes the file hosted on bin.creator54.me"
   else if [ $argv[1] = "-d" ]
     echo "Removing $argv[2] .."
-    ssh $NIX -i ~/.ssh/webserver -t "rm -rf ~/website-stuff/sharedby/$argv[2]" && echo "File is removed !"
+    ssh $NIX -i $server_key -t "rm -rf ~/website-stuff/bin/$argv[2]" && echo "File is removed !"
   else
-    set url "http://sharedby.creator54.me/files/$argv"
-    push $argv ~/website-stuff/sharedby
+    set url "https://bin.creator54.me/$argv"
+    rsync -e "ssh -i $server_key" $argv -aPvz $NIX:~/website-stuff/bin/
     echo "File is shared @ $url" && echo $url | clip
   end
 end
